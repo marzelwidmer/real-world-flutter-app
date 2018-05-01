0b3221db872e84c0a98708670191dc8a4ea38e65
Marcel Widmer <marzelwidmer@gmail.com>
2018-05-01 12:52:41 +0200

    some design stuff

--- lib/main.dart
+++ lib/main.dart
@@ -68,10 +68,12 @@ class RealWoldState extends State<RealWorldApp> {
                           new Container(
                             padding: new EdgeInsets.all(16.0),
                             child: new Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: <Widget>[
                                 new Image.network(video["imageUrl"]),
-                                new Container(height: 6.0,),
-                                new Text(video["name"]),
+                                new Container(height: 8.0,),
+                                new Text(video["name"],
+                                  style: new TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold ),),
                               ],
                             ),
                           ),
