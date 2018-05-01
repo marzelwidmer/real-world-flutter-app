a99409cddd3152d8ae818fa4fd93d05c6862d7c9
Marcel Widmer <marzelwidmer@gmail.com>
2018-05-01 08:53:17 +0200

    impl. onPress funtion on reload button

--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,32 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(new RealWorldApp());
 
-class RealWorldApp extends StatefulWidget{
+class RealWorldApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new RealWoldState();
   }
 }
 
-class RealWoldState extends State<RealWorldApp>{
+class RealWoldState extends State<RealWorldApp> {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
         appBar: new AppBar(
           title: new Text("Real World App Bar"),
+          actions: <Widget>[
+            new IconButton(
+                icon: new Icon(Icons.refresh),
+                onPressed: () {
+                  print("Reloading....");
+                }),
+          ],
         ),
         body: new Center(
           child: new CircularProgressIndicator(),
         ),
-      ), 
+      ),
     );
   }
 }
