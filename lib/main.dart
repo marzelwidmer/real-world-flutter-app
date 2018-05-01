076355976f4eb9b23e0c63d2414d1b36d6e2627f
Marcel Widmer <marzelwidmer@gmail.com>
2018-05-01 12:49:37 +0200

    load network image

--- lib/main.dart
+++ lib/main.dart
@@ -13,26 +13,26 @@ class RealWorldApp extends StatefulWidget {
 
 class RealWoldState extends State<RealWorldApp> {
   var _isLoading = true;
+  var videos;
 
   _fetchData() async {
     print("Attempting to fetch data from network.");
     final url = "https://api.letsbuildthatapp.com/youtube/home_feed";
     final response = await http.get(url);
 
-    if(response.statusCode == 200){
-      // print(response.body);
-
+    if (response.statusCode == 200) {
       final map = json.decode(response.body);
       final videosJson = map["videos"];
+      this.videos = videosJson;
 
-      videosJson.forEach((video){
-          // print(video["name"]);
-          var v = new Video.fromJson(video);
-          print(v.name);
-      });
+      // videosJson.forEach((video){
+      //     // print(video["name"]);
+      //     var v = new Video.fromJson(video);
+      //     print(v.name);
+      // });
 
       setState(() {
-        _isLoading = false;              
+        _isLoading = false;
       });
     }
   }
@@ -49,25 +49,42 @@ class RealWoldState extends State<RealWorldApp> {
                 onPressed: () {
                   print("Reloading....");
                   setState(() {
-                    _isLoading = true;              
+                    _isLoading = true;
                   });
 
-                 _fetchData();
-
+                  _fetchData();
                 }),
           ],
         ),
         body: new Center(
-          //child: new CircularProgressIndicator(),
-          child: _isLoading ? new CircularProgressIndicator() : 
-            new Text("Finished Loading..."),
-        ),
+            child: _isLoading
+                ? new CircularProgressIndicator()
+                : new ListView.builder(
+                    itemCount: this.videos != null ? this.videos.length : 0,
+                    itemBuilder: (context, i) {
+                      final video = this.videos[i];
+                      return new Column(
+                        children: <Widget>[
+                          new Container(
+                            padding: new EdgeInsets.all(16.0),
+                            child: new Column(
+                              children: <Widget>[
+                                new Image.network(video["imageUrl"]),
+                                new Container(height: 6.0,),
+                                new Text(video["name"]),
+                              ],
+                            ),
+                          ),
+                          new Divider()
+                        ],
+                      );
+                      // return new Text("Row $i");
+                    })),
       ),
     );
   }
 }
 
- 
 // {
 // "id": 1,
 // "name": "Instagram Firebase - Learn How to Create Users, Follow, and Send Push Notifications",
@@ -79,7 +96,7 @@ class RealWoldState extends State<RealWorldApp> {
 // "profileImageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/dda5bc77-327f-4944-8f51-ba4f3651ffdf",
 // "numberOfSubscribers": 100000
 // }
-class Video{
+class Video {
   final int id;
   final String name;
   final String link;
@@ -87,7 +104,7 @@ class Video{
 
   Video({this.id, this.name, this.link, this.imageUrl});
 
-  factory Video.fromJson(Map<String, dynamic> json){
+  factory Video.fromJson(Map<String, dynamic> json) {
     return Video(
       id: json['id'],
       name: json['name'],
