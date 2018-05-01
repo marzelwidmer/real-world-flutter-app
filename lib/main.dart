590b8c11dc4a06c2a1141f23ccc494180a8c0b37
Marcel Widmer <marzelwidmer@gmail.com>
2018-05-01 13:11:02 +0200

    refactore

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,6 @@ class RealWoldState extends State<RealWorldApp> {
       final map = json.decode(response.body);
       final videosJson = map["videos"];
       this.videos = videosJson;
-
-      // videosJson.forEach((video){
-      //     // print(video["name"]);
-      //     var v = new Video.fromJson(video);
-      //     print(v.name);
-      // });
-
       setState(() {
         _isLoading = false;
       });
@@ -51,7 +44,6 @@ class RealWoldState extends State<RealWorldApp> {
                   setState(() {
                     _isLoading = true;
                   });
-
                   _fetchData();
                 }),
           ],
@@ -63,55 +55,41 @@ class RealWoldState extends State<RealWorldApp> {
                     itemCount: this.videos != null ? this.videos.length : 0,
                     itemBuilder: (context, i) {
                       final video = this.videos[i];
-                      return new Column(
-                        children: <Widget>[
-                          new Container(
-                            padding: new EdgeInsets.all(16.0),
-                            child: new Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                new Image.network(video["imageUrl"]),
-                                new Container(height: 8.0,),
-                                new Text(video["name"],
-                                  style: new TextStyle(fontSize: 16.0,fontWeight: FontWeight.bold ),),
-                              ],
-                            ),
-                          ),
-                          new Divider()
-                        ],
-                      );
-                      // return new Text("Row $i");
+                      return new VideoCell(video);
                     })),
       ),
     );
   }
 }
 
-// {
-// "id": 1,
-// "name": "Instagram Firebase - Learn How to Create Users, Follow, and Send Push Notifications",
-// "link": "https://www.letsbuildthatapp.com/course/instagram-firebase",
-// "imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/04782e30-d72a-4917-9d7a-c862226e0a93",
-// "numberOfViews": 20000,
-// "channel": {
-// "name": "Lets Build That App",
-// "profileImageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/dda5bc77-327f-4944-8f51-ba4f3651ffdf",
-// "numberOfSubscribers": 100000
-// }
-class Video {
-  final int id;
-  final String name;
-  final String link;
-  final String imageUrl;
+class VideoCell extends StatelessWidget {
 
-  Video({this.id, this.name, this.link, this.imageUrl});
+  final video;
+  VideoCell(this.video);
 
-  factory Video.fromJson(Map<String, dynamic> json) {
-    return Video(
-      id: json['id'],
-      name: json['name'],
-      link: json['link'],
-      imageUrl: json['imageUrl'],
+  @override
+  Widget build(BuildContext context) {
+    return new Column(
+      children: <Widget>[
+        new Container(
+          padding: new EdgeInsets.all(16.0),
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Image.network(video["imageUrl"]),
+              new Container(
+                height: 8.0,
+              ),
+              new Text(
+                video["name"],
+                style:
+                    new TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ),
+        new Divider()
+      ],
     );
   }
 }
