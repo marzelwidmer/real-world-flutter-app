0d7d64f0f5879ae2529318958a8a553613d012a3
Marcel Widmer <marzelwidmer@gmail.com>
2018-05-01 10:00:19 +0200

    parse json convert in object

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 void main() => runApp(new RealWorldApp());
 
@@ -19,7 +20,16 @@ class RealWoldState extends State<RealWorldApp> {
     final response = await http.get(url);
 
     if(response.statusCode == 200){
-      print(response.body);
+      // print(response.body);
+
+      final map = json.decode(response.body);
+      final videosJson = map["videos"];
+      
+      videosJson.forEach((video){
+          // print(video["name"]);
+          var v = new Video.fromJson(video);
+          print(v.name);
+      });
     }
   }
 
@@ -49,6 +59,36 @@ class RealWoldState extends State<RealWorldApp> {
             new Text("Finished Loading..."),
         ),
       ),
+    );
+  }
+}
+
+ 
+// {
+// "id": 1,
+// "name": "Instagram Firebase - Learn How to Create Users, Follow, and Send Push Notifications",
+// "link": "https://www.letsbuildthatapp.com/course/instagram-firebase",
+// "imageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/04782e30-d72a-4917-9d7a-c862226e0a93",
+// "numberOfViews": 20000,
+// "channel": {
+// "name": "Lets Build That App",
+// "profileImageUrl": "https://letsbuildthatapp-videos.s3-us-west-2.amazonaws.com/dda5bc77-327f-4944-8f51-ba4f3651ffdf",
+// "numberOfSubscribers": 100000
+// }
+class Video{
+  final int id;
+  final String name;
+  final String link;
+  final String imageUrl;
+
+  Video({this.id, this.name, this.link, this.imageUrl});
+
+  factory Video.fromJson(Map<String, dynamic> json){
+    return Video(
+      id: json['id'],
+      name: json['name'],
+      link: json['link'],
+      imageUrl: json['imageUrl'],
     );
   }
 }
